8448486099a1ac7f97387582c71342de51372345
ikhsanrhmd <47246812+ikhsanrhmd@users.noreply.github.com>
2019-05-23 16:26:41 +0700

    Add files via upload

---
+++ methods1.rb
@@ -0,0 +1,7 @@
+#method adalah mendefenisikan sesuatu perintah dan dapat dipanggil berlang kali
+def halo_rb (nama, bahasa)
+	puts "hallo #{nama}, ini method pertama #{bahasa}"
+end
+
+halo_rb('isan','ruby')
+halo_rb('naila','kedokteran')
---
+++ return-value.rb
@@ -0,0 +1,10 @@
+def nilai(nilai_a,nilai_b)
+	return nilai_a -nilai_b if nilai_a ==5 
+	nilai_a +nilai_b
+	
+end
+
+hasil1 = nilai(5,4)
+hasil2 = nilai(10,3)
+hasil3 = hasil1 * hasil2
+puts hasil3
